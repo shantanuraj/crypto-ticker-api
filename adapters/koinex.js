4733078c37fea83e6074d189fda598836fc045b7
Shantanu Raj <s@sraj.me>
2018-04-30 16:48:23 +0530

    Support new koinex api

--- adapters/koinex.js
+++ adapters/koinex.js
@@ -10,6 +10,6 @@ const {
 
 const normalizeObj = clone(k => k.toLowerCase(), money.convert('inr', 'usd'));
 
-const adapt = (res) => normalizeObj(pick('data.prices')(res));
+const adapt = (res) => normalizeObj(pick('data.prices.inr')(res));
 
 module.exports = adapt;
